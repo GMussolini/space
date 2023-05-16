8760439ebfd3ef0ce4a199bef73b32fa1ae8bb3c
GMUSSOLINI <gmussolini@provertec.com.br>
2023-05-16 09:23:38 -0300

    feat: mudanças no footer da aplicação.

--- src/Componentes/Footer/Index.jsx
+++ src/Componentes/Footer/Index.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import facebook from "./facebook.svg";
-import twitter from "./twitter.svg";
 import instagram from "./instagram.svg";
 import styles from "./footer.module.scss"
 
@@ -9,28 +7,14 @@ export default function Footer() {
     <footer className={styles.rodape}>
       <div className={styles.rodape__icones}>
         <a
-          href="https://www.facebook.com/AluraCursosOnline/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={facebook} alt="ícone do facebook" />
-        </a>
-        <a
-          href="https://twitter.com/aluraonline?lang=en"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={twitter} alt="ícone do twitter" />
-        </a>
-        <a
-          href="https://www.instagram.com/aluraonline/?hl=en"
+          href="https://www.instagram.com/gihco_/?hl=en"
           target="_blank"
           rel="noreferrer"
         >
           <img src={instagram} alt="ícone do instagram" />
         </a>
       </div>
-      <p>Desenvolvido por Alura</p>
+      <p>Desenvolvido por Giovanni Mussolini</p>
     </footer>
   )
 }
